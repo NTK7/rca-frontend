5301c6ef75fb294ce8a8f7411ee15faf01bc1571
nazhimkalam <nazhimkalam@gmail.com>
2022-06-28 22:37:44 +0530

    commented hero code

--- src/components/DetectorHero/DetectorHero.tsx
+++ src/components/DetectorHero/DetectorHero.tsx
@@ -4,17 +4,17 @@ const DetectorHero = () => {
   return (
     <DetectorHeroStyled>
       <section className="left">
-        <h2>Discover the truth</h2>
+        {/* <h2>Discover the truth</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique officia quas et corporis ullam eius. Magnam natus est suscipit, eaque fugiat autem fuga maxime voluptates officiis sequi iste modi! Quo esse impedit ex obcaecati excepturi odio id doloribus at maxime ad nobis, neque, unde eum?
         </p>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, sequi unde corporis cupiditate quaerat nesciunt deserunt ratione. Aliquid eum quas suscipit optio aperiam obcaecati nesciunt deserunt dolor. Quidem, assumenda facere.
-        </p>
+        </p> */}
       </section>
 
       <section className="right">
-        <img src="banner-2.PNG" alt="" />
+        {/* <img src="banner-2.PNG" alt="" /> */}
       </section>
     </DetectorHeroStyled>
   );
--- src/components/HowItWorks/HowItWorks.tsx
+++ src/components/HowItWorks/HowItWorks.tsx
@@ -6,7 +6,7 @@ const HowItWorks = () => {
     <HowItWorksStyled>
       <h2>How it works? </h2>
       <p>
-        The application will be able to scan the text and classify it as either "Positive" or "Negative" review text. Naviagte to the detection section and paste the text you want to scan and click on the scan button. The result will be displayed. 
+        The application will be able to scan the text and classify it as either "Positive" or "Negative" review text. Naviagte to the detection section and paste the text you want to scan and click on the scan button. The result will be displayed. It will be able to break the sentences and find the sentiment for each sentence from the text.
       </p>
 
       <img src="banner.PNG" alt="" />
